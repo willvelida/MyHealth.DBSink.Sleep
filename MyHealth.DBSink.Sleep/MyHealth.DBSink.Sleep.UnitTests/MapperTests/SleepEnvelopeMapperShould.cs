c696e948b0d1e42e4ed146a5f7c454bef0d6a21b
willvelida <willvelida@hotmail.co.uk>
2021-08-28 17:34:33 +1200

    Adding happy path test for mapper

--- MyHealth.DBSink.Sleep/MyHealth.DBSink.Sleep.UnitTests/MapperTests/SleepEnvelopeMapperShould.cs
+++ MyHealth.DBSink.Sleep/MyHealth.DBSink.Sleep.UnitTests/MapperTests/SleepEnvelopeMapperShould.cs
@@ -1,10 +1,13 @@
-﻿using FluentAssertions;
+﻿using AutoFixture;
+using FluentAssertions;
+using FluentAssertions.Execution;
 using Moq;
 using MyHealth.DBSink.Sleep.Mappers;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using mdl = MyHealth.Common.Models;
 
 namespace MyHealth.DBSink.Sleep.UnitTests.MapperTests
 {
@@ -23,6 +26,24 @@ namespace MyHealth.DBSink.Sleep.UnitTests.MapperTests
             Action sleepEnvelopeMapperAction = () => _sut.MapSleepToSleepEnvelope(null);
 
             sleepEnvelopeMapperAction.Should().Throw<Exception>().WithMessage("No Sleep Document to Map!");
+        }
+
+        [Fact]
+        public void MapSleepToSleepEnvelopeCorrectly()
+        {
+            var fixture = new Fixture();
+            var testSleep = fixture.Create<mdl.Sleep>();
+            testSleep.SleepDate = "2021-08-28";
+
+            var expectedSleepEnvelope = _sut.MapSleepToSleepEnvelope(testSleep);
+
+            using (new AssertionScope())
+            {
+                expectedSleepEnvelope.Should().BeOfType<mdl.SleepEnvelope>();
+                expectedSleepEnvelope.Sleep.Should().Be(testSleep);
+                expectedSleepEnvelope.DocumentType.Should().Be("Sleep");
+                expectedSleepEnvelope.Date.Should().Be(DateTime.Parse(testSleep.SleepDate));
+            }
         }
     }
 }
